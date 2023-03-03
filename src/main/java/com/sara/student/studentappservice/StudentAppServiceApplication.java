ac81ea9f3484c19e8da95729627c5763a3c19406
sarath <saec08531@gmail.com>
2023-03-03 16:52:45 +0530

    Initial commit

---
+++ src/main/java/com/sara/student/studentappservice/StudentAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sara.student.studentappservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAppServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sara/student/studentappservice/StudentAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sara.student.studentappservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
