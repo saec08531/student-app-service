0493b7ae72e8e727af617a92b8ce66139265ba12
saec08531 <saec08531@gmail.com>
2023-03-17 14:30:46 +0530

    Implemented file reading logic

--- src/main/java/com/sara/student/controller/StudentController.java
+++ src/main/java/com/sara/student/controller/StudentController.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -13,25 +15,45 @@ import java.util.List;
 @RequestMapping("api/v1/")
 public class StudentController {
 
-        private final StudentService studentService;
+    private final StudentService studentService;
 
-        public StudentController(StudentService studentService) {
-                this.studentService = studentService;
-        }
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
 
-        @GetMapping
-        public String display() {
-                return "Hello World";
-        }
+    @GetMapping
+    public String display() {
+        return "Hello World";
+    }
 
-        @GetMapping("/students")
-        public List<StudentDto> getStudents() {
-                return studentService.getListOfStudents();
-        }
+    @GetMapping("/students")
+    public List<StudentDto> getStudents() {
+        return studentService.getListOfStudents();
+    }
 
-        @PostMapping
-        public void uploadStudent(@RequestPart(value = "file", required = false) MultipartFile file, @RequestPart StudentDto studentDto) {
-                log.info("Inside upload student method");
-                log.info("Student: {}", studentDto);
+    @PostMapping
+    public void uploadStudent(@RequestPart(value = "file", required = false) MultipartFile file,
+                              @RequestPart StudentDto studentDto) throws FileNotFoundException {
+        log.info("Inside upload student method");
+        log.info("Student: {}", studentDto);
+        if (file.isEmpty()) throw new FileNotFoundException();
+        List<String> list = processFile(file);
+        list.forEach(System.out::println);
+    }
+
+    private List<String> processFile(MultipartFile multipartFile) {
+        BufferedReader br;
+        List<String> result = new ArrayList<>();
+        try {
+            String line;
+            InputStream is = multipartFile.getInputStream();
+            br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {
+                result.add(line);
+            }
+        } catch (IOException e) {
+            log.error("Error while reading the file", e);
         }
+        return result;
+    }
 }
